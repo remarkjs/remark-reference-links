94973bc01e3e66f42d794e98e5c4f8e69b5b694c
Titus Wormer <tituswormer@gmail.com>
2021-08-05 14:51:57 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,69 +1,91 @@
+/**
+ * @typedef {import('mdast').Root} Root
+ * @typedef {import('mdast').Definition} Definition
+ * @typedef {import('mdast').ImageReference} ImageReference
+ * @typedef {import('mdast').LinkReference} LinkReference
+ */
+
 import {visit, SKIP} from 'unist-util-visit'
 
 const own = {}.hasOwnProperty
 
+/**
+ * Plugin to transform links and images into references and definitions.
+ *
+ * @type {import('unified').Plugin<void[], Root>}
+ */
 export default function remarkReferenceLinks() {
-  return transformer
-}
+  return (tree) => {
+    let id = 0
+    /** @type {Record<string, Record<string, Definition>>} */
+    const definitions = Object.create(null)
+    /** @type {string[]} */
+    const existing = []
 
-function transformer(tree) {
-  let id = 0
-  const definitions = Object.create(null)
-  const existing = []
+    // Find existing definitions.
+    visit(tree, 'definition', (node) => {
+      const url = node.url
 
-  // Find existing definitions.
-  visit(tree, 'definition', (node) => {
-    const url = node.url
+      existing.push(node.identifier)
 
-    existing.push(node.identifier)
+      if (!own.call(definitions, url)) {
+        definitions[url] = Object.create(null)
+      }
 
-    if (!own.call(definitions, url)) {
-      definitions[url] = Object.create(null)
-    }
+      definitions[url][node.title || ''] = node
+    })
 
-    definitions[url][node.title] = node
-  })
+    // Transform normal links and images into references and definitions, replaces
+    // the current node, and adds a definition if needed.
+    visit(tree, (node, index, parent) => {
+      if (
+        parent &&
+        typeof index === 'number' &&
+        (node.type === 'image' || node.type === 'link')
+      ) {
+        const url = node.url
+        const title = node.title || ''
+        /** @type {Record<string, Definition>} */
+        const titles = own.call(definitions, url)
+          ? definitions[url]
+          : (definitions[url] = Object.create(null))
+        /** @type {string} */
+        let identifier
 
-  // Transform normal links and images into references and definitions, replaces
-  // the current node, and adds a definition if needed.
-  visit(tree, ['image', 'link'], (node, index, parent) => {
-    const url = node.url
-    const title = node.title
-    const titles = own.call(definitions, url)
-      ? definitions[url]
-      : (definitions[url] = Object.create(null))
-    let identifier
+        if (own.call(titles, title)) {
+          identifier = titles[title].identifier
+        } else {
+          do {
+            identifier = String(++id)
+          } while (existing.includes(identifier))
 
-    if (own.call(titles, title)) {
-      identifier = titles[title].identifier
-    } else {
-      do {
-        identifier = String(++id)
-      } while (existing.includes(identifier))
+          /** @type {Definition} */
+          const definition = {type: 'definition', identifier, title, url}
 
-      const definition = {type: 'definition', identifier, title, url}
+          titles[title] = definition
 
-      titles[title] = definition
+          tree.children.push(definition)
+        }
 
-      tree.children.push(definition)
-    }
+        /** @type {ImageReference|LinkReference} */
+        const replacement =
+          node.type === 'image'
+            ? {
+                type: 'imageReference',
+                identifier,
+                referenceType: 'full',
+                alt: node.alt
+              }
+            : {
+                type: 'linkReference',
+                identifier,
+                referenceType: 'full',
+                children: node.children
+              }
 
-    const replacement =
-      node.type === 'image'
-        ? {
-            type: 'imageReference',
-            identifier,
-            referenceType: 'full',
-            alt: node.alt
-          }
-        : {
-            type: 'linkReference',
-            identifier,
-            referenceType: 'full',
-            children: node.children
-          }
-
-    parent.children[index] = replacement
-    return [SKIP, index]
-  })
+        parent.children[index] = replacement
+        return [SKIP, index]
+      }
+    })
+  }
 }

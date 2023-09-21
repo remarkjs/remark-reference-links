3511249a06bcdf7ffc32669d99e68d5cda413bb4
Titus Wormer <tituswormer@gmail.com>
2023-09-21 09:25:45 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,131 +1,181 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {remark} from 'remark'
 import referenceLinks from './index.js'
 
-test('referenceLinks', (t) => {
-  t.equal(
-    remark()
-      .use(referenceLinks)
-      .processSync(
+test('referenceLinks', async function (t) {
+  await t.test('should work', async function () {
+    assert.equal(
+      String(
+        await remark()
+          .use(referenceLinks)
+          .process(
+            [
+              '[foo](http://example.com "Example Domain"), [foo](http://example.com "Example Domain"), [bar](http://example.com "Example Domain").',
+              '![foo](http://example.com "Example Domain"), ![foo](http://example.com "Example Domain"), ![bar](http://example.com "Example Domain").'
+            ].join('\n')
+          )
+      ),
+      [
+        '[foo][1], [foo][1], [bar][1].',
+        '![foo][1], ![foo][1], ![bar][1].',
+        '',
+        '[1]: http://example.com "Example Domain"',
+        ''
+      ].join('\n')
+    )
+  })
+
+  await t.test(
+    'should number reference links in the order they’re seen',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(referenceLinks)
+            .process(
+              '[foo](http://example.com/1 "Example Domain 1") ![foo](http://example.com/2 "Example Domain 2")'
+            )
+        ),
         [
-          '[foo](http://example.com "Example Domain"), ',
-          '[foo](http://example.com "Example Domain"), ',
-          '[bar](http://example.com "Example Domain").',
-          '\n',
-          '![foo](http://example.com "Example Domain"), ',
-          '![foo](http://example.com "Example Domain"), ',
-          '![bar](http://example.com "Example Domain").',
-          '\n'
-        ].join('')
+          '[foo][1] ![foo][2]',
+          '',
+          '[1]: http://example.com/1 "Example Domain 1"',
+          '',
+          '[2]: http://example.com/2 "Example Domain 2"',
+          ''
+        ].join('\n')
       )
-      .toString(),
-    [
-      '[foo][1], [foo][1], [bar][1].\n![foo][1], ![foo][1], ![bar][1].',
-      '[1]: http://example.com "Example Domain"\n'
-    ].join('\n\n'),
-    'should work'
+    }
   )
 
-  t.equal(
-    remark()
-      .use(referenceLinks)
-      .processSync(
-        [
-          '[foo](http://example.com/1 "Example Domain 1") ',
-          '![foo](http://example.com/2 "Example Domain 2")',
-          '\n'
-        ].join('')
-      )
-      .toString(),
-    [
-      '[foo][1] ![foo][2]',
-      '[1]: http://example.com/1 "Example Domain 1"',
-      '[2]: http://example.com/2 "Example Domain 2"\n'
-    ].join('\n\n'),
-    'reference links are numbered in order they’re seen'
-  )
-
-  t.equal(
-    remark()
-      .use(referenceLinks)
-      .processSync(
+  await t.test(
+    'should not coalesce reference links with existing identifiers',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(referenceLinks)
+            .process(
+              [
+                '# Hello!',
+                'This is a [thing][1] and [that](bravo.com) is a [thing][3]',
+                'and [stuff](delta.com)',
+                '',
+                '[1]: alpha.com',
+                '[3]: charlie.com'
+              ].join('\n')
+            )
+        ),
         [
           '# Hello!',
-          'This is a [thing][1] and [that](bravo.com) is a [thing][3]\nand [stuff](delta.com)',
+          '',
+          'This is a [thing][1] and [that][2] is a [thing][3]',
+          'and [stuff][4]',
+          '',
           '[1]: alpha.com',
-          '[3]: charlie.com\n'
-        ].join('\n\n')
+          '',
+          '[3]: charlie.com',
+          '',
+          '[2]: bravo.com',
+          '',
+          '[4]: delta.com',
+          ''
+        ].join('\n')
       )
-      .toString(),
-    [
-      '# Hello!',
-      'This is a [thing][1] and [that][2] is a [thing][3]\nand [stuff][4]',
-      '[1]: alpha.com',
-      '[3]: charlie.com',
-      '[2]: bravo.com',
-      '[4]: delta.com\n'
-    ].join('\n\n'),
-    'reference links don’t coalesce with existing identifiers'
+    }
   )
 
-  t.equal(
-    remark()
-      .use(referenceLinks)
-      .processSync('[This](alpha.com "alpha").\n\n[That](alpha.com "bravo").\n')
-      .toString(),
-    [
-      '[This][1].',
-      '[That][2].',
-      '[1]: alpha.com "alpha"',
-      '[2]: alpha.com "bravo"\n'
-    ].join('\n\n'),
-    'should support same URLs with different titles'
-  )
-
-  t.equal(
-    remark()
-      .use(referenceLinks)
-      .processSync(
+  await t.test(
+    'should support same URLs with different titles',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(referenceLinks)
+            .process('[This](alpha.com "alpha").\n\n[That](alpha.com "bravo").')
+        ),
         [
-          '[This](alpha.com "alpha").',
+          '[This][1].',
+          '',
+          '[That][2].',
+          '',
+          '[1]: alpha.com "alpha"',
+          '',
+          '[2]: alpha.com "bravo"',
+          ''
+        ].join('\n')
+      )
+    }
+  )
+
+  await t.test(
+    'should not reuse the same url as a definition if it has a different title',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(referenceLinks)
+            .process(
+              [
+                '[This](alpha.com "alpha").',
+                '',
+                '[1]: alpha.com "bravo"',
+                '[2]: alpha.com "charlie"'
+              ].join('\n')
+            )
+        ),
+        [
+          '[This][3].',
+          '',
           '[1]: alpha.com "bravo"',
-          '[2]: alpha.com "charlie"\n'
-        ].join('\n\n')
+          '',
+          '[2]: alpha.com "charlie"',
+          '',
+          '[3]: alpha.com "alpha"',
+          ''
+        ].join('\n')
       )
-      .toString(),
-    [
-      '[This][3].',
-      '[1]: alpha.com "bravo"',
-      '[2]: alpha.com "charlie"',
-      '[3]: alpha.com "alpha"\n'
-    ].join('\n\n'),
-    'should not reuse the same url as a definition if it has a different title'
+    }
   )
 
-  t.equal(
-    remark()
-      .use(referenceLinks)
-      .processSync(
+  await t.test(
+    'should use existing definitions if they exist',
+    async function () {
+      assert.equal(
+        String(
+          await remark()
+            .use(referenceLinks)
+            .process(
+              [
+                '[This](alpha.com "alpha").',
+                '',
+                '[That](alpha.com).',
+                '',
+                '[Thut](bravo.com "charlie").',
+                '',
+                '[this]: alpha.com "alpha"',
+                '[thut]: bravo.com "delta"'
+              ].join('\n')
+            )
+        ),
         [
-          '[This](alpha.com "alpha").',
-          '[That](alpha.com).',
-          '[Thut](bravo.com "charlie").',
+          '[This][this].',
+          '',
+          '[That][1].',
+          '',
+          '[Thut][2].',
+          '',
           '[this]: alpha.com "alpha"',
-          '[thut]: bravo.com "delta"\n'
-        ].join('\n\n')
+          '',
+          '[thut]: bravo.com "delta"',
+          '',
+          '[1]: alpha.com',
+          '',
+          '[2]: bravo.com "charlie"',
+          ''
+        ].join('\n')
       )
-      .toString(),
-    [
-      '[This][this].',
-      '[That][1].',
-      '[Thut][2].',
-      '[this]: alpha.com "alpha"',
-      '[thut]: bravo.com "delta"',
-      '[1]: alpha.com',
-      '[2]: bravo.com "charlie"\n'
-    ].join('\n\n'),
-    'should use existing definitions if they exist'
+    }
   )
-
-  t.end()
 })
